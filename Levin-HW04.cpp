d15e5421332565ed8bc6c1c58afd77aef1a2f919
logikill99 <logikill17@gmail.com>
2022-09-29 15:31:35 -0500

    wrote Levin-HW04.cpp

---
+++ Levin-HW04.cpp
@@ -0,0 +1,13 @@
+//Program Name: Small World
+//Purpose: Write, compile, and run a simple C++ program
+//Author: Matthew Levin
+//Date Last Modified: 9/29/2022
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Hello World" << "Matthew Levin" << endl;
+    return 0;
+}
